688a4b3d724a9a920d00abd6ef169b50d1633b23
Anatoliy Dudar <gigsollt@gmail.com>
2026-03-30 15:56:03 +0300

    updated to include basic rest framework config

--- game_library_manager/game_library_manager/urls.py
+++ game_library_manager/game_library_manager/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 from .views import current_datetime
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", current_datetime),
+    path("api-auth/", include("rest_framework.urls")),
 ]
