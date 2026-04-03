09c69ce8ff19aa2f03c78c9c84960d2c597b49c8
Anatoliy Dudar <gigsollt@gmail.com>
2026-04-03 13:07:23 +0300

    feat: added basic viewsets

--- game_library_manager/api/views.py
+++ game_library_manager/api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from core.models import Game, Genre, BacklogEntry
+from .serializers import GenreSerializer
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class GenreViewSet(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class GameViwSet(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
